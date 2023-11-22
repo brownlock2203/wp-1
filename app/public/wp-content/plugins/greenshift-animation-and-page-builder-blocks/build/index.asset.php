5559e4958a22b13ab3f83961bffdefb5c095c132
brownlock2203 <brandonmedehou2203@gmail.com>
2023-11-22 14:32:06 +0100

    First wp-1

---
+++ app/public/wp-content/plugins/greenshift-animation-and-page-builder-blocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '72a65fb3c5a1193f02b7');
